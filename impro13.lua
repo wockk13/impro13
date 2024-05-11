56253fbfbe33b89b2e210797e2f595749dd0c00c
wockk13 <165584263+wockk13@users.noreply.github.com>
2024-05-11 14:36:42 -0400

    impro13.lua

---
+++ impro13.lua
@@ -0,0 +1,3 @@
+script_key="IvoqDpvuqugmELkcORnZkLLJkTLRpsYx";
+getgenv().SmallEdition = true -- if the gui is huge for u, set this to  false  no capitals or nothing
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/7cb85479e6370eac42e07e73bc2d8378.lua"))()
